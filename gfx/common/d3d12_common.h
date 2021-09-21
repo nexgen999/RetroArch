78c091e485e020fcfa83304211fedf94145952c7
Nun-z <54733172+Nun-z@users.noreply.github.com>
2021-09-20 21:34:01 -0700

    Terrible azure compiler fix

--- gfx/common/d3d12_common.h
+++ gfx/common/d3d12_common.h
@@ -64,6 +64,16 @@ D3D12_RESOURCE_ALLOCATION_INFO ( STDMETHODCALLTYPE *GetResourceAllocationInfo )(
    _In_  UINT numResourceDescs,
    _In_reads_(numResourceDescs)  const D3D12_RESOURCE_DESC *pResourceDescs);
 
+D3D12_HEAP_PROPERTIES ( STDMETHODCALLTYPE *GetCustomHeapProperties )( 
+   ID3D12Device * This,
+   _In_  UINT nodeMask,
+   D3D12_HEAP_TYPE heapType);
+
+HRESULT ( STDMETHODCALLTYPE *StorePipeline )( 
+   ID3D12PipelineLibrary * This,
+   _In_opt_  LPCWSTR pName,
+   _In_  ID3D12PipelineState *pPipeline);
+
 static INLINE ULONG D3D12Release(void* object)
 {
    return ((ID3D12Object*)object)->lpVtbl->Release((ID3D12Object*)object);
