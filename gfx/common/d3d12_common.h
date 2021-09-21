b58a341d1f2e48780f87a89f6b4b9d2ea3731bc9
Nun-z <54733172+Nun-z@users.noreply.github.com>
2021-09-20 21:18:10 -0700

    Update d3d12_common.h

--- gfx/common/d3d12_common.h
+++ gfx/common/d3d12_common.h
@@ -52,6 +52,12 @@ typedef ID3D12DebugCommandList*                   D3D12DebugCommandList;
 #endif
 typedef ID3D12InfoQueue*                          D3D12InfoQueue;
 
+D3D12_RESOURCE_ALLOCATION_INFO(STDMETHODCALLTYPE* GetResourceAllocationInfo)(
+   ID3D12Device* This,
+   _In_  UINT visibleMask,
+   _In_  UINT numResourceDescs,
+   _In_reads_(numResourceDescs)  const D3D12_RESOURCE_DESC* pResourceDescs);
+
 static INLINE ULONG D3D12Release(void* object)
 {
    return ((ID3D12Object*)object)->lpVtbl->Release((ID3D12Object*)object);
